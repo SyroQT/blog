fbbb964b50de24bc33a954cd81eeb3c3cf8d411d
Titas Janušois <titas.januson@gmail.com>
2025-05-13 18:17:45 +0200

    Add hero img change style location

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import styles from './Hero.module.css'
+import styles from '../../styles/components/Hero.module.css'
 
 function Hero() {
     const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/titas-dev-blog.appspot.com/o/images%2Fblogs%2Fkatedraa-tiny.jpg?alt=media&token=db22994f-2b5d-40fb-8254-f1218fdd1ac4'
