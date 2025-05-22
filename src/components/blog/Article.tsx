0d8f166f97daa6a286f1e4bb20ce656f320f960c
Titas Janušois <titas.januson@gmail.com>
2025-05-22 16:46:29 +0200

    Add blog text support

--- src/components/blog/Article.tsx
+++ src/components/blog/Article.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { BlogPost } from '@/lib/firebase/blogs'
 import ReactMarkdown from 'react-markdown'
 import { Title } from '../ui/Title'
-
+import styles from '../../styles/blog/Article.module.css'
 interface ArticleProps {
     post: BlogPost
 }
@@ -26,56 +26,14 @@ export function Article({ post }: ArticleProps) {
                     <Title>
                         <h1>{post.title}</h1>
                     </Title>
-                    {/* <h1 className="text-4xl md:text-5xl font-bold text-white text-center px-4">
-                        {post.title}
-                    </h1> */}
-
                 </div>
             </div>
-        </article >
+
+            {/* Markdown Content */}
+            <div className={`${styles.articleBlogText} 
+            prose prose-lg max-w-none dark:prose-invert font-montserrat`}>
+                <ReactMarkdown>{post.content}</ReactMarkdown>
+            </div>
+        </article>
     )
 }
-
-
-// <article className="max-w-4xl mx-auto px-4 py-8">
-// {/* Header Section */}
-// <header className="mb-8">
-//     <h1 className="text-3xl md:text-4xl font-bold mb-4">{post.title}</h1>
-//     <p className="text-lg text-gray-600 mb-4">{post.description}</p>
-//     <div className="flex items-center gap-4 text-sm text-gray-500">
-//         <span>•</span>
-//         <span>{post.readTime} read</span>
-//     </div>
-// </header>
-
-// {/* Main Image */}
-// <div className="relative w-full aspect-[16/9] mb-8">
-//     <Image
-//         src={post.imageUrl || imageFallBack}
-//         alt={post.title}
-//         fill
-//         className="object-cover rounded-lg"
-//         priority
-//         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-//     />
-// </div>
-
-// {/* Content */}
-// <div className="prose prose-lg max-w-none">
-//     <ReactMarkdown>{post.content}</ReactMarkdown>
-// </div>
-
-// {/* Tags */}
-// {post.tags && post.tags.length > 0 && (
-//     <div className="mt-8 flex flex-wrap gap-2">
-//         {post.tags.map((tag) => (
-//             <span
-//                 key={tag}
-//                 className="px-3 py-1 bg-gray-100 text-gray-700 rounded-full text-sm"
-//             >
-//                 {tag}
-//             </span>
-//         ))}
-//     </div>
-// )}
-// </article>
