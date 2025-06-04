be8b1b5a78a2b5aa327162f4fc53748b0e57cc93
Titas Janušonis <46631449+SyroQT@users.noreply.github.com>
2025-06-04 20:46:34 +0200

    Centralize revalidation interval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Hero } from '@/components/hero/Hero'
 import { BlogPosts } from '@/components/cards/BlogPosts'
+import { REVALIDATE_INTERVAL } from '@/lib/constants'
+
+// Revalidate the homepage every hour to refresh blog listings
+export const revalidate = REVALIDATE_INTERVAL
 
 export default function Home() {
   return (
