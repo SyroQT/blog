389fca0e2362003bc83986a712a86454119766be
Titas Janušonis <46631449+SyroQT@users.noreply.github.com>
2025-06-04 09:14:18 +0200

    Fix duplicate grid class in RootLayout

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -31,7 +31,7 @@ interface RootLayoutProps {
 export function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={`${courierPrime.variable} ${inter.variable} ${roboto.variable} ${montserrat.variable}`}>
-      <body className="grid grid h-screen grid-cols-[1fr_auto] grid-rows-[2fr_1fr] gap-2">
+      <body className="grid h-screen grid-cols-[1fr_auto] grid-rows-[2fr_1fr] gap-2">
         <main className="col-span-1 font-montserrat">
           {children}
         </main>
